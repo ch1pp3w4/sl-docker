5c82969db6e8969ae027ad6312e370b7d92eb944
ch1pp3w4 <ch1pp3w4@udev03>
2024-05-11 17:57:27 -0400

    Remove scripts from tracking

--- scripts/docker_stop_the_always.sh
+++
@@ -1,1 +0,0 @@
-docker stop $(docker ps -a -q) & docker update --restart=no $(docker ps -a -q) & sudo systemctl restart docker
--- scripts/init.sql
+++
@@ -1,3 +0,0 @@
--- CREATE USER sql-ledger WITH PASSWORD 'uqwye6-02-=1jhja123#$11';
--- CREATE DATABASE sql-ledger;
--- GRANT ALL PRIVILEGES ON DATABASE sql-ledger TO sql-ledger;
